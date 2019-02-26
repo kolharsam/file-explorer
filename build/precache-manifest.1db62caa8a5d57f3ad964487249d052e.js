5364f697e132beacb927dc89786a4b907077642c
Sam K <kolhar730@gmail.com>
2019-02-26 21:05:22 +0530

    added build files

---
+++ build/precache-manifest.1db62caa8a5d57f3ad964487249d052e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7ad4e469422e885c7bec",
+    "url": "/static/js/main.7ad4e469.chunk.js"
+  },
+  {
+    "revision": "5115c3f305eb463ec26a",
+    "url": "/static/js/2.5115c3f3.chunk.js"
+  },
+  {
+    "revision": "7ad4e469422e885c7bec",
+    "url": "/static/css/main.11d61ffc.chunk.css"
+  },
+  {
+    "revision": "b0d8144532a75417b1aa5ebdfd537c91",
+    "url": "/index.html"
+  }
+];
